f6dc73bd68fe6ddc17b122c0c1d01cdac8498066
Olgica <olgicau@oipinsurtech.com>
2026-06-08 14:06:28 +0200

    Changes addes.

--- bad_style.py
+++ bad_style.py
@@ -1,15 +1,11 @@
-# This file has intentional style issues
+def calculate_area(length, width):
+    result = length * width
+    if result > 100:
+        print("Large area!")
+    return result
 
 
-def badly_formatted(x, y, z):
-    a = 1
-    b = 2
-    if a == b:
-        print("equal")
-    return x + y + z
-
-
-result = badly_formatted(1, 2, 3)
-d = {"key1": "value1", "key2": "value2"}
-for i in range(10):
-    print(i)
+my_length = 10
+my_width = 5
+area = calculate_area(my_length, my_width)
+print(f"The area is: {area}")
