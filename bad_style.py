e09f8012bc437115135e406ac9a85c8b4e128e05
Olgica <olgicau@oipinsurtech.com>
2026-06-08 13:44:58 +0200

    Task 1.3: Fixed code style with black and flake8 - removed unused imports

---
+++ bad_style.py
@@ -0,0 +1,15 @@
+# This file has intentional style issues
+
+
+def badly_formatted(x, y, z):
+    a = 1
+    b = 2
+    if a == b:
+        print("equal")
+    return x + y + z
+
+
+result = badly_formatted(1, 2, 3)
+d = {"key1": "value1", "key2": "value2"}
+for i in range(10):
+    print(i)
